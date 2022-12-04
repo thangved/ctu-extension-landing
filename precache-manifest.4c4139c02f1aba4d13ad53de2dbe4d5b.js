b91e83a786fd9da1c2bf48af6529c041453710d8
thangved <thangved02@gmail.com>
2022-12-04 04:05:35 +0000

    Deploying to gh-pages from @ thangved/ctu-management-system-extension-landing@84e39660a39766681490fe1241fed75249d45852 🚀

---
+++ precache-manifest.4c4139c02f1aba4d13ad53de2dbe4d5b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fe7cd7e242b31de8253ffae6ad404b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db872956ae75950e719e",
+    "url": "/static/css/main.793f35e2.chunk.css"
+  },
+  {
+    "revision": "1fa9ae7ae95005390e07",
+    "url": "/static/js/2.8c3de0e1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.8c3de0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db872956ae75950e719e",
+    "url": "/static/js/main.351a0292.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "59ea4a05b4a1e45cc8bc3dcfdebdccfb",
+    "url": "/static/media/logo.59ea4a05.png"
+  },
+  {
+    "revision": "0e9af3041a083f0e323ee9f6330cade1",
+    "url": "/static/media/video-placeholder.0e9af304.jpg"
+  }
+]);

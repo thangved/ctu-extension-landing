cb341795350fda7997401b2cec53a7e27325809f
Kim Minh Thang <thangved02@gmail.com>
2024-07-29 14:34:56 +0700

    chore: Add .env.example and .eslintrc.cjs files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from "react";
 import { useLocation, Switch } from "react-router-dom";
 import AppRoute from "./utils/AppRoute";
 import ScrollReveal from "./utils/ScrollReveal";
+import ReactGA from "react-ga";
 
 // Layouts
 import LayoutDefault from "./layouts/LayoutDefault";
@@ -11,13 +12,22 @@ import Home from "./views/Home";
 import Docs from "./views/Docs";
 import Privacy from "./views/Privacy";
 
+ReactGA.initialize(import.meta.env.VITE_APP_GA_CODE);
+
+const trackPage = (page) => {
+	ReactGA.set({ page });
+	ReactGA.pageview(page);
+};
+
 const App = () => {
 	const childRef = useRef();
 	let location = useLocation();
 
 	useEffect(() => {
+		const page = location.pathname;
 		document.body.classList.add("is-loaded");
 		childRef.current.init();
+		trackPage(page);
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [location]);
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import { createBrowserHistory } from "history";
 import React from "react";
-import { hydrate, render } from "react-dom";
-import { Router } from "react-router-dom";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 //import './App.css';
 import "./assets/scss/style.scss";
@@ -13,23 +12,9 @@ const history = createBrowserHistory();
 
 const rootElement = document.getElementById("root");
 
-if (rootElement.hasChildNodes()) {
-	hydrate(
-		<Router history={history}>
-			<App />
-		</Router>,
-		rootElement
-	);
-} else {
-	render(
-		<Router history={history}>
-			<App />
-		</Router>,
-		rootElement
-	);
-}
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+	<Router history={history}>
+		<App />
+	</Router>,
+	rootElement
+);

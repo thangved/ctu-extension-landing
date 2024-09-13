a7cd360f7bc2e34e236e8a4a24a6eb73a90723c7
Kim Minh Thang <thangved02@gmail.com>
2024-09-13 15:59:06 +0700

    fix: interactive elements should be focusable

--- src/components/elements/Modal.jsx
+++ src/components/elements/Modal.jsx
@@ -62,8 +62,8 @@ const Modal = ({
 
 	return (
 		show && (
-			<div {...props} className={classes} onClick={handleClose} role="button">
-				<div className="modal-inner" onClick={stopProgagation} role="button">
+			<div {...props} className={classes} onClick={handleClose} aria-hidden>
+				<div className="modal-inner" onClick={stopProgagation} aria-hidden>
 					{video ? (
 						<div className="responsive-video">
 							{videoTag === "iframe" ? (
